65eafa34e6ee7c1254b0d0d75d9f7d570eb6e141
YFROBOTZL <yfrobot-zl@foxmail.com>
2021-12-18 20:46:37 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -833,29 +833,6 @@ namespace YFSENSORS {
         let checksum = 0;
     }
 
-    /**
-     * Fixed voice broadcast module function : Continuous Play.
-     * @param vbmPin pin. eg: DigitalPin.P2
-     * @param serial_number voice serial number list array.
-     */
-    //% group="Output"
-    //% blockId=YFSENSORS_voiceBroadcastModuleFunContPlay weight=92 blockGap=15
-    //% block="voice broadcast %vbmPin| continuous play %serial_number"
-    //% vbmPin.fieldEditor="gridpicker" vbmPin.fieldOptions.columns=4
-    //% inlineInputMode=inline
-    export function voiceBroadcastModuleFunContPlay(vbmPin: DigitalPin, serial_number: Array<number>): void {
-        let checksum = 0;
-        // voiceBroadcastModuleSendDataWithS(vbmPin, YFSENSORS.OTPFixedVoiceFun2.HeadCode); // 头码
-        // checksum += YFSENSORS.OTPFixedVoiceFun2.HeadCode;
-        // for (let index = 0; index < serial_number.length; index++) {
-        //     voiceBroadcastModuleSendData(vbmPin, serial_number[index]); // 语音列表码
-        //     checksum += serial_number[index];
-        // }
-        // voiceBroadcastModuleSendData(vbmPin, YFSENSORS.OTPFixedVoiceFun2.TailCode); // 尾码
-        // checksum += YFSENSORS.OTPFixedVoiceFun2.TailCode;
-        // voiceBroadcastModuleSendData(vbmPin, (checksum && 0xFF)); // 校验和
-    }
-
     ///////////////////// Output - MP3 audio playback module ///////////////////////
     /**
      * Connects the MP3 audio playback module to the specified pin.
